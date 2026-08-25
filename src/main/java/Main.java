129985c2fc9c9cb91a37562b9bb32d692b410b25
whatsername07 <alexwanklyn@gmail.com>
2026-08-25 17:37:52 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
+        while (true) {
+            System.out.print("$ ");
+            String input = scanner.nextLine();
+            System.out.println(input + ": command not found");
+        }
     }
 }
