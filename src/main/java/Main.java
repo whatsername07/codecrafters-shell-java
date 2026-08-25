2ebcc0070bc23ddb8479534deb60aa6e9beaf296
whatsername07 <alexwanklyn@gmail.com>
2026-08-25 19:21:43 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        String[] commands = {"echo", "exit"};
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
-            if (input.equals("exit")) {
+            if (input.contains("type")) {
+                String typeInput = input.substring(5);
+                if (Arrays.asList(commands).contains(typeInput)) {
+                    System.out.println(typeInput + " is a shell builtin");
+                } else {
+                    System.out.println(typeInput + ": not found");
+                }
+            }
+            else if (input.equals("exit")) {
+                scanner.close();
                 break;
             }
             else if (input.contains("echo")) {
