d3b877fae0cb2b980b9bfcff905fccdd3f33152c
whatsername07 <alexwanklyn@gmail.com>
2026-08-25 17:41:08 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,9 @@ public class Main {
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
+            if (input.equals("exit")) {
+                break;
+            }
             System.out.println(input + ": command not found");
         }
     }
